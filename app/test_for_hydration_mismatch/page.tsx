c51f7840a8f90d7561beced510cbc3cc0ac436a0
lkb <tmwlak@gmail.com>
2023-05-11 15:05:01 +0900

    -

--- app/test_for_hydration_mismatch/page.tsx
+++ app/test_for_hydration_mismatch/page.tsx
@@ -1,14 +1,21 @@
 'use client'
-import { useId } from "react"
+import { useEffect, useId } from "react"
 
 export default function A() {
     const someid = useId()
     console.log({someid});
+
+    useEffect(() => {
+        console.log(
+            document.querySelector(`#${CSS.escape(someid)}`)
+        );        
+    }, []);
+
     return (
         <>
             {/* Warning: Prop `id` did not match. Server: "0.5971144041520453" Client: "0.577785766430871" */}
-            <div className="" id={Math.random().toString()}>
-            {/* <div className="" id={someid}> */}
+            {/* <div className="" id={Math.random().toString()}> */}
+            <div className="" id={someid}>
                 this is home1111
                 {/* Error: Text content does not match server-rendered HTML. */}
                 {/* {Math.random()} */}
