b8484cc04b4311b216b243628c72c29d980b48c3
lkb <tmwlak@gmail.com>
2023-05-11 15:03:24 +0900

    -

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,13 @@
+import { useId } from "react"
+
 export default function A() {
+    const someid = useId()
+    console.log({someid});
     return (
         <>
-            <div className="">
-                this is home
+            {/* <div className="" id={Math.random().toString()}> */}
+            <div className="" id={someid}>
+                this is home1111
             </div>
         </>
     )
---
+++ app/test_for_hydration_mismatch/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { useId } from "react"
+
+export default function A() {
+    const someid = useId()
+    console.log({someid});
+    return (
+        <>
+            {/* Warning: Prop `id` did not match. Server: "0.5971144041520453" Client: "0.577785766430871" */}
+            <div className="" id={Math.random().toString()}>
+            {/* <div className="" id={someid}> */}
+                this is home1111
+                {/* Error: Text content does not match server-rendered HTML. */}
+                {/* {Math.random()} */}
+            </div>
+        </>
+    )
+}
